edef2a7f148471a53730c83e1fc36e22de36ab73
joshuaoben <olonadejoshua@gmail.com>
2018-01-13 19:08:58 +0100

    login page setup

--- resources/views/user/user-login.blade.php
+++ resources/views/user/user-login.blade.php
@@ -3,28 +3,34 @@
 @section('content')
 
 
-	<section class="login">
-		<div class="container">
-			<h3> {{ $label }} </h3>
-			<div class="row pt20 col-sm-6">
-	    <div class="col-sm-10">
-	      <input type="email" placeholder="Input">
+	<div class="listing-objects has-bg-image" data-bg-color="f5f5f5">
+    <div class="container">
+        <div class="row" style="">
+            <div class="col-md-8" style="margin:0 auto;  float:none;">
+                <h2> Login</h2>
+                <form class="form login">
+                    <div class="row pt30">
+                        <?php if(!empty($err_msg)){echo $err_msg;} ?>
+                        
+                        <div class="col-sm-7">
+                             <label>Email Address</label>
+                          <input name="email" type="email" placeholder="Email" class="form-control" required>
+                        </div>
+                        <div class="col-sm-7">
+                            <label>Password</label>
+                          <input name="password" type="password" placeholder="Password" class="form-control" required>
+                        </div>
+                        <div class="col-sm-12">
+                            <button  class="btn mb20 btn-small btn-transparent-primary"><i class="fa fa-user"></i>Login</button>
+                        </div>    
 
-	    </div>
-	    <div class="col-sm-10">
-	      <div class="custom-select-box">
-	        <select name="name" id="selectbox">
-	          <option value="0">Dropdown</option>
-	          <option value="1">Dropdown1</option>
-	          <option value="2">Dropdown2</option>
-	        </select>
-	      </div>
+                    </div>
+                 </form>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+</div>
 
-	    </div>
-
-	  </div>
-		</div>
-
-	</section>
 	 
 @stop
