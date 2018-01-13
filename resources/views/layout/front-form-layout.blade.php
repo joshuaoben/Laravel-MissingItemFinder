ec5b68d0864af18f06f98ab5f2967a9709461a83
joshuaoben <olonadejoshua@gmail.com>
2018-01-13 17:14:43 +0100

    header and footer sorting

---
+++ resources/views/layout/front-form-layout.blade.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>{{ $title }}</title>
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,300,400,600,700%7CDroid+Serif:300,400,700,400italic">
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href="http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="/css/owl.carousel.css">
+
+
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+<div id="main-wrapper" class="homepage">
+  <div class="toolbar">
+    <div class="uou-block-1a blog">
+      <div class="container">
+        <ul class="quick-nav">
+          <li><a href="about.html">About Us</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+        </ul>
+
+        <ul class="social">
+          <li><a href="#" class="fa fa-facebook"></a></li>
+          <li><a href="#" class="fa fa-twitter"></a></li>
+          <li><a href="#" class="fa fa-google-plus"></a></li>
+        </ul>
+
+        <ul class="authentication">
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Register</a></li>
+        </ul>
+      </div>
+    </div> <!-- end .uou-block-1a -->
+  </div> <!-- end toolbar -->
+
+  <div class="header-nav">
+      <div class="box-shadow-for-ui">
+
+        <div class="uou-block-2b icons">
+          <div class="container">
+            <a href="#" class="logo"><img src="img/logo.png" alt=""></a>
+            <a href="#" class="mobile-sidebar-button mobile-sidebar-toggle"><span></span></a>
+
+            <nav class="nav">
+              <ul class="sf-menu">
+                <li class="active"><a href="index.html"><i class="fa fa-home"></i> Home</a>
+                  <ul class="demo-menu">
+                    <li class="active"><a href="index.html">Home 1</a></li>
+                    <li><a href="index2.html">Home 2</a></li>
+                  </ul>
+                </li>
+                
+                <li>
+                  <a href="index_restaurant.html"><i class="fa fa-cutlery"></i>  Restaurants</a>
+                  <ul class="demo-menu">
+                    <li><a href="index_restaurant.html">Restaurant Index</a></li>
+                    <li><a href="single_restaurant.html">Restaurant Single</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-cogs"></i> Features</a>
+                  <ul class="demo-menu">
+                    <li><a href="box-variation1.html">Box Variation 1</a></li>
+                    <li><a href="box-variation2.html">Box Variation 2</a></li>
+                    <li><a href="box-variation3.html">Box Variation 3</a></li>
+                    <li><a href="gui-kit.html">GUI kit</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div> <!-- end .uou-block-2b -->
+
+      </div>
+  </div> <!-- edn header-navm -->
+  
+@yield('content')
+
+
+ 
+</div>
+
+
+<!-- Scripts -->
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script src="js/jquery-2.1.3.min.js"></script>
+<script src="js/plugins/superfish.min.js"></script>
+<script src="js/jquery.ui.min.js"></script>
+<script src="js/plugins/rangeslider.min.js"></script>
+
+<script src="js/plugins/jquery.flexslider-min.js"></script>
+
+<script src="js/uou-accordions.js"></script>
+
+<script src="js/uou-tabs.js"></script>
+<script src="js/plugins/select2.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/gmap3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/scripts.js"></script>
+
+</body>
+</html>
+
+<!-- Localized -->
--- resources/views/user/user-login.blade.php
+++ resources/views/user/user-login.blade.php
@@ -1,7 +1,30 @@
-@extends('layout.front-entry-layout')
+@extends('layout.front-form-layout')
 
 @section('content')
 
-<h2> {{ $label }} </h2>
 
+	<section class="login">
+		<div class="container">
+			<h3> {{ $label }} </h3>
+			<div class="row pt20 col-sm-6">
+	    <div class="col-sm-10">
+	      <input type="email" placeholder="Input">
+
+	    </div>
+	    <div class="col-sm-10">
+	      <div class="custom-select-box">
+	        <select name="name" id="selectbox">
+	          <option value="0">Dropdown</option>
+	          <option value="1">Dropdown1</option>
+	          <option value="2">Dropdown2</option>
+	        </select>
+	      </div>
+
+	    </div>
+
+	  </div>
+		</div>
+
+	</section>
+	 
 @stop
